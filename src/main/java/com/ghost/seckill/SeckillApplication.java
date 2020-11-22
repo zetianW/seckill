edd8cb54b0bc6c655f86153884e5a62e5ee064c6
zetian <zetian_w@163.com>
2020-11-22 16:35:00 +0800

    创建项目

---
+++ src/main/java/com/ghost/seckill/SeckillApplication.java
@@ -0,0 +1,16 @@
+package com.ghost.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ghost
+ */
+@SpringBootApplication
+public class SeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghost/seckill/SeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
